01bebc4d24a1e04d4f7797a139a27f9b35e2ad0a
Черепахин Василий <vasi.che@gmail.com>
2023-05-11 16:05:41 +0500

    add Stat.java

---
+++ src/main/java/demo1/Stat.java
@@ -0,0 +1,55 @@
+package demo1;
+
+import java.util.Objects;
+
+public class Stat {
+    private String grp ="";
+    private String type ="";
+    private Long num = 0L;
+    private Long weight = 0l;
+
+    public String getGrp() {
+        return grp;
+    }
+
+    public void setGrp(String grp) {
+        this.grp = grp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getNum() {
+        return num;
+    }
+
+    public void setNum(Long num) {
+        this.num = num;
+    }
+
+    public Long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Long weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stat stat = (Stat) o;
+        return Objects.equals(grp, stat.grp) && Objects.equals(type, stat.type) && Objects.equals(num, stat.num) && Objects.equals(weight, stat.weight);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(grp, type, num, weight);
+    }
+}
