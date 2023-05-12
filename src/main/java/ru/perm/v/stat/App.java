56450a9aa65717ede8a5ba39850d78b863c8ab89
Черепахин Василий <vasi.che@gmail.com>
2023-05-12 13:19:13 +0500

    main обработка ввода

--- src/main/java/ru/perm/v/stat/App.java
+++ src/main/java/ru/perm/v/stat/App.java
@@ -1,8 +1,24 @@
 package ru.perm.v.stat;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
 public class App {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter path file for load:");
+        String file = in.next();
+        if (file.equals("exit")) {
+            exit(0);
+        }
+        LoaderStat loaderStat = new LoaderStat();
+        try {
+            loaderStat.readFromFile(file);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
