8e734896c55e4611630c29c4170ce38da4a4b768
Черепахин Василий <vasi.che@gmail.com>
2023-05-12 19:52:56 +0500

    Add MaxMinWeightReport.java, ResultMinMaxWeight.java

--- src/main/java/ru/perm/v/stat/report/parts/MaxMinWeightReport.java
+++ src/main/java/ru/perm/v/stat/report/parts/MaxMinWeightReport.java
@@ -1,13 +1,19 @@
 package ru.perm.v.stat.report.parts;
 
+import ru.perm.v.stat.results.ResultMinMaxWeight;
+
 import java.util.List;
 
-public class MaxMinWeightReport implements IPrintReport{
+public class MaxMinWeightReport implements IPrintReport {
     Long minWeight = 0L;
     Long maxWeight = 0L;
 
+    public MaxMinWeightReport(ResultMinMaxWeight resultMinMaxWeight) {
+
+    }
+
     @Override
-    public List<String> print() {
+    public List<String> getBody() {
         return List.of("");
     }
 }
---
+++ src/main/java/ru/perm/v/stat/results/ResultMinMaxWeight.java
@@ -0,0 +1,22 @@
+package ru.perm.v.stat.results;
+
+public class ResultMinMaxWeight {
+    Long min = 0L;
+    Long max = 0L;
+
+    public Long getMin() {
+        return min;
+    }
+
+    public void setMin(Long min) {
+        this.min = min;
+    }
+
+    public Long getMax() {
+        return max;
+    }
+
+    public void setMax(Long max) {
+        this.max = max;
+    }
+}
