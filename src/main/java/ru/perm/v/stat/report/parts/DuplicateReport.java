de2bc2ff8d38a94d679351829a8d1c7a7f32f91b
Черепахин Василий <vasi.che@gmail.com>
2023-05-12 19:51:47 +0500

    Add DuplicateReport.java

--- src/main/java/ru/perm/v/stat/report/parts/DuplicateReport.java
+++ src/main/java/ru/perm/v/stat/report/parts/DuplicateReport.java
@@ -1,10 +1,17 @@
 package ru.perm.v.stat.report.parts;
 
+import ru.perm.v.stat.results.ResultDuplicates;
+
 import java.util.List;
 
 public class DuplicateReport implements IPrintReport {
+    ResultDuplicates resultDuplicates;
+    public DuplicateReport(ResultDuplicates resultDuplicates) {
+        this.resultDuplicates = resultDuplicates;
+    }
+
     @Override
-    public List<String> print() {
+    public List<String> getBody() {
         return List.of("");
     }
 }
---
+++ src/main/java/ru/perm/v/stat/results/ResultDuplicates.java
@@ -0,0 +1,18 @@
+package ru.perm.v.stat.results;
+
+import ru.perm.v.stat.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultDuplicates {
+    List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
