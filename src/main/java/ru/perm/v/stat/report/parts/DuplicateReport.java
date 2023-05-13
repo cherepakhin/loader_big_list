7dae0b79d3cb21305785bfc786f0e8c0780beb93
Черепахин Василий <vasi.che@gmail.com>
2023-05-13 18:41:46 +0500

    Format DuplicateReport

--- src/main/java/ru/perm/v/stat/report/parts/DuplicateReport.java
+++ src/main/java/ru/perm/v/stat/report/parts/DuplicateReport.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class DuplicateReport implements IPrintReport {
     private final ResultDuplicates resultDuplicates;
-
+    private final String delimeterLine = "-----------------------------------------------\n";
     public DuplicateReport(ResultDuplicates resultDuplicates) {
         this.resultDuplicates = resultDuplicates;
     }
@@ -17,13 +17,15 @@ public class DuplicateReport implements IPrintReport {
     @Override
     public List<String> getBody() {
         List<String> body = new ArrayList<>();
-        body.add("Duplicates:\n");
+        body.add("==================Duplicates==================\n");
         for (Duplicate duplicate : resultDuplicates.getDuplicates()) {
             body.add(String.format("Duplicates by Group: %s and Type: %s\n", duplicate.getGrp(),duplicate.getType()));
+            body.add(delimeterLine);
             for (Product product : duplicate.getProducts()) {
                 body.add(String.format("Product: grp=%s, nnum=%s, type=%s, weight=%s\n",
                     duplicate.getGrp(), product.getNum(), product.getType(), product.getWeight()));
             }
+            body.add(delimeterLine);
         }
         return body;
     }
